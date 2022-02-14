ef8b31c494b46df235c5bdfc8f49fe1da7e292d4
Arthur Bauville <a.bauville@gmail.com>
2022-02-14 13:51:30 +0900

    basic implementation of the stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Questions Controller
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    msg = params[:question]
+
+    @coach_answer = if msg == 'I am going to work'
+                      'Great!'
+                    elsif msg[-1] == '?'
+                      'Silly question, get dressed and go to work!.'
+                    else
+                      "I don't care, get dressed and go to work!"
+                    end
+  end
+end
